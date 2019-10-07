012634c5fd55e7809836bbe5eb6b4301536249bd
Pierre Bretéché <pbreteche@dawan.fr>
2019-10-07 17:31:33 +0200

    clean code

--- src/UrlReader.php
+++ src/UrlReader.php
@@ -5,17 +5,23 @@ class UrlReader
 
     public function parse()
     {
-        echo $_SERVER['REQUEST_URI'];
-
-        $uriParts = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
-        $annonceUri = count($uriParts) === 2
-                && $uriParts[0] === 'annonce'
-                && is_numeric($uriParts[1]);
+        // découpe de l'url sur les "/"
+        $path = trim($_SERVER['REQUEST_URI'], '/');
+        $uriParts = explode('/', $path);
         
-        if ($annonceUri) {
+        if ($this->match($uriParts)) {
             return $uriParts[1];
         }
 
+        // pas de format d'url trouvé
         throw new Exception('URL non reconnue');
+    }
+
+    private function match(array $parts)
+    {
+        // url de la form "annonce/<numéro>" ?
+        return count($parts) === 2
+                && $parts[0] === 'annonce'
+                && is_numeric($parts[1]);
     }
 }
