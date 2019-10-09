c38ead29586ca51f8c7837c84219e2c22b4d5417
Pierre Bretéché <pbreteche@dawan.fr>
2019-10-09 14:40:49 +0200

    template system

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 require __DIR__.'/../vendor/autoload.php';
+define('APP_DIR', __DIR__.'/..');
 
 $app = new App\Application();
 
--- src/html/Annonce.php
+++ src/html/Annonce.php
@@ -9,19 +9,8 @@ class Annonce
 
     public function build(\App\Annonce $annonce): string
     {
-        return <<<EOT
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Annonces</title>
-</head>
-<body>
-  <h1>$annonce->title</h1><div>$annonce->content</div>
-</body>
-</html>
-EOT;
+        ob_start();
+        include APP_DIR.'/templates/annonce.phtml';
+        return ob_get_clean();
     }
 }
